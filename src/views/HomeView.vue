e83a2832ce50dadd06bb6a32ebf39c68a9de1a2d
abdelrhman ashraf <mido59478@gmail.com>
2023-03-13 05:37:49 +0200

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -221,7 +221,7 @@
               data-aos-duration="2000"
             >
               <div class="imgBox">
-                <img :src="skill.image" :alt="skill.name" />
+                <img :src="getImageUrl(skill.image)" :alt="skill.name" />
               </div>
               <p>{{ skill.name }}</p>
               <v-tooltip max-width="200" activator="parent" location="top">{{
@@ -436,7 +436,7 @@ window.onscroll = () => {
 };
 
 function getImageUrl(urlName) {
-  return new URL(urlName, import.meta.url).href;
+  return new URL(`../assets/Icons/${urlName}`, import.meta.url).href;
 }
 </script>
 
